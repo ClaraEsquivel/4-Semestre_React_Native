4794b7614773b1bd2d10f7db25b8f1fa2589f791
Clara Esquivel <160122503+ClaraEsquivel@users.noreply.github.com>
2025-10-10 10:44:58 -0300

    Add files via upload

--- claraapp/app/index.tsx
+++ claraapp/app/index.tsx
@@ -1,15 +1,42 @@
-import { Text, View } from "react-native";
+//Importação
+import {View, Text, StyleSheet} from 'react-native';
 
-export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+//Corpo do Aplicativo
+export default function App(){
+
+
+  return(
+    <View style = {styles.central}>
+
+      <Text style = {styles.titulo}>APP Barbearia</Text>
+      <Text style = {styles.titulo2}>A melhor barbearia da CBX!</Text>
+
     </View>
+
+
   );
 }
+
+
+//Area de Estilização - CSS
+const styles = StyleSheet.create({
+  central:{
+    backgroundColor: '#191970',
+    flex:1, //ocupa toda a tela
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  titulo:{
+    color: 'white',
+    fontSize: 50,
+    fontWeight: '600'
+  },
+
+  titulo2:{
+    color: 'yellow',
+    fontSize: 30,
+    fontWeight: '500'
+  },
+  
+})
